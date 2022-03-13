2251b76d0152c0dbaf16a5cc3c2cc900166f1ba5
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-03-13 14:35:14 +0100

    footer css and layout in jsx set up

---
+++ film_rental/src/Components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+import './Footer.css';
+
+const Footer = () => {
+
+    return (
+        <div className="designFooter">
+            Film Rental Service - 2022
+        </div>
+    )
+
+}
+
+export default Footer;
