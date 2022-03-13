14c747e033f12b78585e0d675997dd04ca0e82f5
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-03-13 16:14:07 +0100

    filled out the content of the container folders (home, login, movie details, profile, register and search results)

---
+++ film_rental/src/Containers/Home/Home.jsx
@@ -0,0 +1,99 @@
+import React, {useState, useEffect} from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { connect } from 'react-redux';
+import { MOVIE_DETAIL } from '../../redux/types';
+import {raiz} from '../../utiles';
+import './Home.css';
+import { Card } from 'antd';
+import 'antd/dist/antd.css';
+
+const Home = (props) => {
+
+    const [films, setFilms] = useState([]);
+    let navigate = useNavigate();
+    const { Meta } = Card;
+
+    useEffect(()=>{
+        //No es correcto realizar el try catch en el useEffect
+        //dado que el useEffect es en si un proceso con un callback, meter un proceso
+        //asíncrono traería problemas y React no lo permite, por ello, llamamos a una funcion
+        //que habremos hecho nosotros y se encargará de ello
+
+        traePelis();
+    },[]);
+
+    //useEffect custom para el hook films
+
+    useEffect(()=>{
+        console.log("vaya, , films ha cambiado, ", films);
+    },[films]);
+
+    const traePelis = async () => {
+
+        try {
+
+            let res = await axios.get("https://lug-movie-club.herokuapp.com/movie-db/new");
+
+            //Una vez han venido los datos del backend, nosotros, lo siguiente que haremos para que no se pierdan
+            //será setear esos datos en el hook, haciendo que las peliculas estén disponibles 
+            //para los return del componente.
+
+            setTimeout(()=>{
+
+                setFilms(res.data.results);
+            },2000);
+
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    const escogePelicula = (pelicula) => {
+        
+        console.log(pelicula);
+        //Guardamos la pelicula escogida en redux
+        props.dispatch({type:MOVIE_DETAIL, payload: pelicula});
+
+
+        //Redirigimos a movieDetail con navigate
+        navigate("/moviedetail");
+    }
+ 
+    if(films[0]?.id != undefined){
+        return(
+            <div className="designRooster">
+
+                {
+                    //Voy a mapear las películas
+                    films.map(pelicula => {
+                        //a cada elemento que voy a mapear
+                        //le brindo un KEY (obligatorio) que lo distinguirá de
+                        //el resto de elementos
+                        return (
+                            //Al mapear, cada elemento que se itera del array (en este caso pelicula es ese elemento),
+                            //si le hacemos propiedad onclick y pasamos el elemento como argumento,
+                            //a esa funcion le va a llegar el objeto que hayamos clickado entero
+                            <div className="cardPelicula" key={pelicula.id} onClick={()=>escogePelicula(pelicula)}>
+                                <img className="fotoCard" src={raiz + pelicula.poster_path} alt={pelicula.title}/>
+                                <p>{pelicula.overview}</p>
+                            </div>
+                            
+                        )
+                    })
+                }
+                
+            </div>
+        )
+    }else{
+        return (
+            <div className='designHome'>
+                <div className="marginLoader">
+                    <img src={require('../../img/loader.gif')} alt="cargador"/>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default connect()(Home);
---
+++ film_rental/src/Containers/MovieDetail/MovieDetail.jsx
@@ -0,0 +1,47 @@
+import React, {useEffect, useState} from 'react';
+import { connect } from 'react-redux';
+import {useNavigate} from 'react-router-dom';
+import Rent from '../../Components/Rent/Rent';
+import {raiz} from '../../utiles';
+
+import './MovieDetail.css';
+
+const MovieDetail = (props) => {
+
+    let navigate = useNavigate();
+    
+
+
+    useEffect(()=> {
+        //Compruebo si hay datos de la película escogida en redux, en caso de NO
+        //haber datos, redirijo a HOME.
+
+        if(props.search?.title === undefined){
+            navigate("/");
+        }
+    });
+
+        return(
+            <div className='designFilm'>
+                <div className="filmDetailHalf">
+                    <div className="dataFilm">{props.search?.title}</div>
+                    <div className="dataFilm">{props.search?.synopsis}</div>
+                    <div className="dataFilm">
+                        {
+                            //EN CASO DE QUE TOKEN SEA TRUE, SI SE INCLUYE EL ELEMENTO RENT
+                            props.credentials.token && <Rent id={props.search.id} token={props.credentials.token} idUser={props.credentials.usuario.id}/>
+                        }
+                    </div>
+                </div>
+                <div className="filmDetailHalf">
+                    <img className="cartel" src={raiz + props.search.image} alt={props.search.title}/></div>    
+            </div>
+        )
+   
+}
+
+export default connect((state) => ({
+    credentials: state.credentials,
+    search : state.search.film
+}))(MovieDetail);
+
