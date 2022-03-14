4a4ee1ed520f43ab58add67fb784e4fe4cf014fa
callumgordon90 <callumjosephiaingordon@gmail.com>
2022-03-14 12:29:28 +0100

    added the register button and changed the utiles document

--- film_rental/src/utiles.js
+++ film_rental/src/utiles.js
@@ -1,1 +1,42 @@
 export const raiz = "https://image.tmdb.org/t/p/w185";
+
+
+export const checkError = (type,value) => {
+
+
+    switch(type) {
+
+        case 'email' :
+
+            if (! /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(value) ) {
+                
+                return "Introduce un e-mail válido";
+            }else{
+                return "ok";
+            };
+            
+        
+        case 'nombre': 
+
+            if (! /[a-z]/gi.test(value) ) {
+                return "Introduce un nombre válido";
+            }else{
+                return "ok";
+            };
+
+        
+        case 'telefono':
+
+            if (! /[\d()+-]/g.test(value) ) {
+                return "Introduce un telefono válido";
+            }else{
+                return "ok";
+            };
+
+        default:
+            return "ok";
+        
+
+    }
+};
+
